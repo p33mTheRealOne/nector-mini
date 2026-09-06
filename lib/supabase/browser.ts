360d56ec9f9eed5350fafa1f77fb7e92e2a4ab3a
P33M <98473379+p33mTheRealOne@users.noreply.github.com>
2026-09-06 19:54:04 +0700

    Delete lib directory

--- lib/supabase/browser.ts
+++
@@ -1,9 +0,0 @@
-// lib/supabase/browser.ts
-import { createBrowserClient } from '@supabase/ssr';
-
-export function supabaseBrowser() {
-  return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  );
-}
